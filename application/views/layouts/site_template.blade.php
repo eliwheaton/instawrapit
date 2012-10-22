75113ac2a62c51069d32413c48ccdd2cfae5d370
Dave West <dave@twosevenzero.com>
2012-10-21 22:38:53 -0400

    Launchrock title

--- application/views/layouts/site_template.blade.php
+++ application/views/layouts/site_template.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
+    <title>Instawrap.it | Your Pictures. Wrapping Paper. Pure Awesome.</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
 
