2c4fcd12461a3d65a6d5b6651b8a6b9174fdb804
Dave West <dave@twosevenzero.com>
2012-10-28 10:06:53 -0400

    Top nav changes

--- application/views/layouts/_header.blade.php
+++ application/views/layouts/_header.blade.php
@@ -5,13 +5,21 @@
         </div>
         <nav class="eight columns top-bar">
             <ul class="right">
-               <li>
-                    @if ( !Session::has('oauth2-access-token-instagram') )
-                        <a href="/oauth2/instagram">Sign in with Instagram</a>
-		    @else
- 			<a href="#">{{ Session::get('username') }}</a>
-		    @endif
+            @if ( !Session::has('oauth2-access-token-instagram') )
+                <li>
+                    <a href="/oauth2/instagram">Sign in with Instagram</a>
                 </li>
+            @else
+                <li class="has-dropdown">
+ 			        <a href="#">{{ Session::get('username') }}</a>
+
+                    <ul class="dropdown">
+                        <li>
+                            <a href="/user/logout">Logout</a>
+                        </li>
+                    </ul>
+                </li>
+            @endif
             </ul>
         </nav>
     </header>
